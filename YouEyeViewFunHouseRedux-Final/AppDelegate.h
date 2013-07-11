865caf4b99ad508a94ef341d60334e0de898817f
Umut Kanbak <uk@kanbak.net>
2013-07-10 21:25:31 -0500

    Initial Commit

---
+++ YouEyeViewFunHouseRedux-Final/AppDelegate.h
@@ -0,0 +1,19 @@
+//
+//  AppDelegate.h
+//  YouEyeViewFunHouseRedux-Final
+//
+//  Created by Umut Kanbak on 7/10/13.
+//  Copyright (c) 2013 Umut Kanbak. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class ViewController;
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (strong, nonatomic) ViewController *viewController;
+
+@end
---
+++ YouEyeViewFunHouseRedux-Final/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  YouEyeViewFunHouseRedux-Final
+//
+//  Created by Umut Kanbak on 7/10/13.
+//  Copyright (c) 2013 Umut Kanbak. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
